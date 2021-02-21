7537d4af3a2ba27be48f36b36d7156f2df3f9ab2
Lance Hietpas <lancehietpas@gmail.com>
2021-02-20 18:39:47 -0600

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,18 +4,8 @@ export default class footer extends Component {
   render() {
     return (
       <div className="site-footer" id="footer">
-        <div className="container">
-          <ul className="social">
-          <li>
-                <a
-                  className="fab fa-instagram"
-                  href={data.instagram}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                ></a>
-              </li>
-      </ul>
-          <span>{this.props.siteName}</span>
+       
+          
         </div>
       </div>
     );
